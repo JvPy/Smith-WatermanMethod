c8f30c4f178e8993988a54d8abbcaac1eba0b13c
JvPy <jvmartinellis@gmail.com>
2019-05-10 22:59:54 -0300

    Added FASTA reader module

--- C/global.c
+++ C/global.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <string.h>
 
@@ -10,7 +11,57 @@ clock_t begin;
 clock_t end;
 double timeE;
 
-end = clock();
-timeE = (double)(end - begin) / CLOCKS_PER_SEC;
-printf("\nTempo de execucao: %.3f segundos.\nFIM DO PROCESSO.\n\n", timeE);
-return 0;
+int main(void){
+  int i, j, k;         //Conters
+  int score, a, b, c;   //Score
+
+  char str1[261], str2[326];          //DNA strings
+  char *T_Score, *T_backtrack; //Matrix - Score and backtrack
+
+  FILE *fastaFile;
+
+  puts("Score:\n");
+  printf("Match: %i  Mismatch: %i  Gap: %i.\n\n", match, mismatch, gap);
+  begin = clock();
+
+  fastaFile=fopen ("gen.fasta","r");  /* o arquivo se chama exemplo.bin e está localizado no diretório corrente */
+  if (!fastaFile)
+    printf ("Erro na abertura do arquivo.");
+  else
+    printf("Fasta aberto\n");
+
+  char line [61]; /* or other suitable maximum line size */
+  i = 0;
+  while(fgets(line, sizeof line, fastaFile) != NULL){ /* read a line */
+    // printf("Linha: %i ", i);
+    //fputs ( line, stdout ); /* write the line */
+    if(line[0] != '>'){
+      if(i == 1){
+        printf("S1 ");
+        printf("%li\t", strlen(line));
+        fputs(line, stdout);
+        strcat(str1, line);
+      }
+      if(i == 2){
+        printf("S2 ");
+        printf("%li\t", strlen(line));
+        fputs(line, stdout);
+        strcat(str2, line);
+      }
+    } else {
+      printf("Sequencia %i: \n", i);
+      i++;
+    }
+  }
+
+  puts("");
+  printf("STR1:\n%s\n\n", str1);
+  printf("STR2:\n%s\n\n", str2);
+
+
+
+  end = clock();
+  timeE = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("\nTempo de execucao: %.3f segundos.\nFIM DO PROCESSO.\n\n", timeE);
+  return 0;
+}
