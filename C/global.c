e1b7ce9feee341c720a8b2d7413b19f1f6a5d7d0
JvPy <jvmartinellis@gmail.com>
2019-06-25 21:19:44 -0300

    Working NW method without backtracking matrix

--- C/global.c
+++ C/global.c
@@ -7,30 +7,28 @@
 #define mismatch -3
 #define gap -4
 
-#define linha 262
-#define coluna 327
+#define backtrackMatch -5
+#define backtrackMismatch 3
+#define backtrackGap 4
+
+// #define linha 262
+// #define coluna 327
+#define linha 5
+#define coluna 7
 
 clock_t begin;
 clock_t end;
 double timeE;
 
-// Tarefinha:
-// S1: gaattcagtta
-// S2: ggatcga
-//
-// Considerando
-// Match 5
-// Mismatch -3
-// Gap -4
-
 int main(void){
   int i, j, k;         //Conters
   int score, a, b, c;   //Score
 
-  char str1[linha] = {0}, str2[coluna] = {0};          //DNA strings
-  // char str2[coluna] = {'g','g','a','t','c','a','g'};
-  // char s1[linha] = {'g','a','a','t','t','c','a','g','t','t','a'};
-  char T_Score[linha][coluna], T_backtrack[linha][coluna];        //Matrix - Score and backtrack
+  // char str1[linha] = {0}, str2[coluna] = {0};          //DNA strings
+   char tScore[linha][coluna];        //Matrix - Score and backtrack
+
+  char str1[5] = { 'A','C','G','C','\0'};
+  char str2[7] = { 'G','A','C','T','A','C','\0'};
 
   FILE *fastaFile;
 
@@ -38,110 +36,102 @@ int main(void){
   printf("Match: %i  Mismatch: %i  Gap: %i.\n\n", match, mismatch, gap);
   begin = clock();
 
-  fastaFile=fopen ("gen.fasta","r");
-  if (!fastaFile)
-    printf ("Erro na abertura do arquivo.");
-  else
-    printf("Fasta aberto\n");
-
-  char line [128];
-  i = 0;
-  while(fgets(line, sizeof line, fastaFile) != NULL){
-
-    if(line[0] != '>'){
-      if(i == 1){
-        printf("S1 ");
-        printf("%li\t", strlen(line));
-        fputs(line, stdout);
-        strcat(str1, line);
-      }
-      if(i == 2){
-        printf("str2 ");
-        printf("%li\t", strlen(line));
-        fputs(line, stdout);
-        strcat(str2, line);
-      }
-    } else {
-      i++;
-    }
-  }
+  // fastaFile=fopen ("gen.fasta","r");
+  // if (!fastaFile)
+  //   printf ("Erro na abertura do arquivo.");
+  // else
+  //   printf("Fasta aberto\n");
+  //
+  // char line [128];
+  // i = 0;
+  // while(fgets(line, sizeof line, fastaFile) != NULL){
+  //
+  //   if(line[0] != '>'){
+  //     if(i == 1){
+  //       printf("S1 ");
+  //       printf("%li\t", strlen(line));
+  //       fputs(line, stdout);
+  //       strcat(str1, line);
+  //     }
+  //     if(i == 2){
+  //       printf("str2 ");
+  //       printf("%li\t", strlen(line));
+  //       fputs(line, stdout);
+  //       strcat(str2, line);
+  //     }
+  //   } else {
+  //     i++;
+  //   }
+  // }
 
   puts("");
   printf("STR1:\n%s\n\n", str1);
   printf("STR2:\n%s\n\n", str2);
+
   //Metodo
-  //Similaraty
-  for(i = 0; i < linha; i++)
-    T_Score[i][0] = i * gap;
+  //Similaridade
+  tScore[0][0] = 0;
 
-  for(j = 1; j < coluna; j++)
-    T_Score[0][j] = j * gap;
+	for(i = 0; i < linha-1; i++)
+		tScore[i][0] = i * gap;
 
-  for(i = 1; i < linha; i++){
-    for(j = 1; j < coluna; j++){
+	for(j = 0; j < coluna-1; j++)
+		tScore[0][j] = j * gap;
 
-      if(str1[i-1] == str2[j-1])
-        score = match;
+  for(i = 1; i < linha-1; i++){
+    for(j = 1; j < coluna-1; j++){
+
+      score = str1[i-1] == str2[j-1] ? match : mismatch;
+
+			a = tScore[i-1][j-1] + score;
+			b = tScore[i-1][j] + gap;
+			c = tScore[i][j-1] + gap;
+
+      if(a >= b && a >= c)
+        tScore[i][j] = a;
+      else if(b > a && b >= c)
+        tScore[i][j] = b;
       else
-        score = mismatch;
-
-      a = T_Score[i-1][j-1] + score;
-      b = T_Score[i-1][j] + gap;
-      c = T_Score[i][j-1] + gap;
-
-      if(a >= b && a >= c){
-        T_Score[i][j] = a;
-        T_backtrack[i][j] = 'd';
-      } else if(b > a && b >= c){
-        T_Score[i][j] = T_Score[i-1][j] + gap;
-        T_backtrack[i][j] = 'u';
-      } else {
-        T_Score[i][j] = T_Score[i][j-1] + gap;
-        T_backtrack[i][j] = 'l';
-      }
+        tScore[i][j] = c;
     }
   }
 
-  for(i = 0; i < linha; i++)
-    T_backtrack[i][0] = 'u';
+  printf("i: %d, j: %d\n\n", i, j);
 
-  for(j = 1; j < coluna; j++)
-    T_backtrack[0][j] = 'l';
+  k = linha >= coluna ? linha : coluna;
 
-  T_backtrack[0][0] = '.';
+  // for(j = 0; j < coluna-1; j++)
+  //   printf("\t%c", str2[j]);
+  // puts("");
+  // for(i = 0; i < linha-1; i++){
+  //   printf("%c\t", str1[i]);
+  //   for(j = 0; j < coluna-1; j++){
+  //     printf("%d\t", tScore[i][j]);
+  //   }
+  //   puts("");
+  // }
 
-  //backtrack
-  i = strlen(str1)-1;
-  j = strlen(str2)-1;
 
-  if(linha >= coluna)
-    k = linha;
-  else
-    k = coluna;
+  while(i > 0 || j > 0){
+    score = str1[i-1] == str2[j-1] ? backtrackMatch : backtrackMismatch;
 
-  while(k >= 0){
-    printf("%ix%i\t%c\t", i, j, T_backtrack[i][j]);
-    if(T_backtrack[i][j] == 'd'){
-      printf("|%c\t%c|\n", str2[j-1], str1[i-1]);
+    a = tScore[i-1][j-1] + score;
+    b = tScore[i-1][j] + backtrackGap;
+    c = tScore[i][j-1] + backtrackGap;
+
+    printf("i: %d, j: %d\n\n", i, j);
+    if(a <= b && a <= c){
+      printf("Match\t\t|%c\t%c|\n", str1[i], str2[j]);
       i--;
       j--;
-      k--;
-    } else if(T_backtrack[i][j] == 'l'){
-      printf("|%c\t-|\n", str2[j-1]);
+    } else if(b < a && b <= c){
+      printf("Gap Linha\t|%c\t-|\n", str1[i-2]);
       j--;
-      k--;
-    } else if(T_backtrack[i][j] == 'u'){
-      printf("|-\t%c|\n", str1[i-1]);
+    } else {
+      printf("Gap Coluna\t|-\t%c|\n", str2[j-2]);
       i--;
-      k--;
-    } else if(T_backtrack[i][j] == '.'){
-      break;
     }
-
-    if(j < 0)
-      break;
   }
-  // return 0;
 
   end = clock();
   timeE = (double)(end - begin) / CLOCKS_PER_SEC;
